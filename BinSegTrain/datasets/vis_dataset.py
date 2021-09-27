5e54ae6f5674679f101e83974d0e74dad41c0449
FengYidan <fengyidan1995@126.com>
2021-09-27 16:08:28 +0800

    Update vis_dataset.py

--- BinSegTrain/datasets/vis_dataset.py
+++ BinSegTrain/datasets/vis_dataset.py
@@ -5,6 +5,9 @@ import os
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import DatasetCatalog, MetadataCatalog
 import argparse
+##
+import low_solidity_support as loso
+##
 
 def get_dicts(json_path):
     with open(json_path, 'r') as f:
@@ -17,7 +20,7 @@ def get_dicts(json_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--phase', default='train')
-    parser.add_argument('--name', default='01')
+    parser.add_argument('--name', default='10')
     args = parser.parse_args()
     json_path = os.path.join(args.name,'json', args.phase+'.json')
     dicts = get_dicts(json_path)
@@ -32,6 +35,15 @@ if __name__ == "__main__":
         out = visualizer.draw_dataset_dict(d)
         vis_out = out.get_image()[:,:,::-1]/255.0
         vis_out = vis_out.astype('float32')
+        ##
+        if loso.IS_LOW:
+            for obj in d['annotations']:
+                c_x,c_y,_,_,_ = obj["bbox"]
+                len_off = len(obj['offset'])/2 - 1
+                for i in range(int(len_off)):
+                    des = (int(c_x + 50*obj['offset'][0] + 200.0 * obj['offset'][2*(i+1)+1]), int(c_y + 50*obj['offset'][1] + 200.0 * obj['offset'][2*(i+1)]))
+                    vis_out = cv2.arrowedLine(img= vis_out, pt1=(c_x,c_y), pt2=des,color=[1.0,0,0],thickness=2)
+        ##      
         imshow(vis_out)
         show()
 
